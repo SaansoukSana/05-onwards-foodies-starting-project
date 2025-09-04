62945d4803f763c112fd59a5d4fd0ff29577418b
SaansoukSana <mick_sana@hotmail.com>
2025-09-04 08:20:07 -0500

    103

--- app/page.js
+++ app/page.js
@@ -4,19 +4,19 @@ import classes from "./page.module.css"
 export default function Home() {
   return (
     <>
-      <header className={classes.header}></header>
-      <div className={classes.slideshow}>
-      </div>
-      <div>
-      <div className={classes.hero}> 
-        <h1>NextLevel Foodies for NextLevel Foodies</h1>
-        <p>Taste & share food from all around the world.</p>
-      </div>
-        <div className={classes.cta} >
-          <Link href="/community">Join the Community</Link>
-          <Link href="/meals">Explore Meals</Link>
+      <header className={classes.header}>
+        <div className={classes.slideshow}></div>
+        <div>
+          <div className={classes.hero}> 
+            <h1>NextLevel Foodies for NextLevel Foodies</h1>
+            <p>Taste & share food from all around the world.</p>
+          </div>
+          <div className={classes.cta} >
+            <Link href="/community">Join the Community</Link>
+            <Link href="/meals">Explore Meals</Link>
+          </div>
         </div>
-      </div>
+      </header>
       <main>
         <section className={classes.section}>
           <h2>How it works</h2>
